c69679b2a3b6600b1c763e5ea140734c8a782754
mbechmann <53899664+mbechmann@users.noreply.github.com>
2020-03-26 23:16:42 +0100

    -

--- dataproject/dataproject.py
+++ dataproject/dataproject.py
@@ -1,17 +1,23 @@
-def only_keep_municipalities(df):
-    """ delete all non-municipalities
+import pandas as pd
+
+def long(df, var):
+    """ Convert dataframe from wide to long format
 
     Args:
-        df (pd.DataFrame): pandas dataframe with the column "municipality" as a string
-
+        df (pd.DataFrame): pandas dataframe with the column "Country/Region" as a string
+        var (str): string that defines variable (confirmed, deaths, recovered)
+        
     Returns:
         df (pd.DataFrame): pandas dataframe
 
     """ 
     
-    for val in ['Region', 'Province', 'All Denmark']:
-        
-        I = df.municipality.str.contains(val)
-        df = df.loc[I == False] # keep everything else
+    df_long = pd.DataFrame(df.unstack())
+    df_long.reset_index(inplace=True)
+    df_long.rename(columns = {'level_0':'date'}, inplace=True)
+    df_long.rename(columns = {'Unnamed':var, 0:var}, inplace=True)
+    df_long = pd.melt(df_long,id_vars = ('Country/Region', 'date'), value_vars = var)
+    df_long.rename(columns = {'value':var}, inplace=True)
+
     
-    return df
+    return df_long
